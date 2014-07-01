d72be26a9a905a6a75236af8778ffcf886e3e3e6
Matt Petrie <matthew.petrie@gmail.com>
2014-07-01 10:50:51 -0700

    note mode fixes

--- sudoku.js
+++ sudoku.js
@@ -28,9 +28,8 @@ var SHOWN = Sudoku.SHOWN = [
 ];
 
   var Board = Sudoku.Board = function(){
-    this.grid = this.generateGrid();
+    this.grid = this._generateGrid();
     this.populate();
-    this.inputSquares = this.findInputSquares();
     this.selectedNumber = null;
     this.deleteMode = false;
     this.conflictMode = false;
@@ -38,150 +37,24 @@ var SHOWN = Sudoku.SHOWN = [
     this.noteMode = false;
   };
 
-  Board.prototype.generateGrid = function(){
-    var grid = new Array(9);
-    for(var i = 0; i < grid.length; i++){
-      grid[i] = new Array(9);
+  Board.prototype.addNote = function(id, target){
+    if(!this.selectedNumber){
+      return;
     }
-    return grid;
-  };
-
-  Board.prototype.findInputSquares = function(){
-    return _.filter((_.flatten(this.grid)), function(square){
-      return square.revealed == 0;
-    });
-  };
-
-  Board.prototype.setCoord = function(arr, val){
-    if(typeof this.grid[arr[0]][arr[1]] == "object"){
-      this.grid[arr[0]][arr[1]].curValue = val;
+    var square = this.grid[id[0]][id[1]];
+    if(square.notes[this.selectedNumber]){
+      delete square.notes[this.selectedNumber]
     } else {
-      this.grid[arr[0]][arr[1]] = val;
+      square.notes[this.selectedNumber] = true;
     }
-  };
-
-  Board.prototype.render = function(el){
-    for(var i = 0; i < this.grid.length; i++){
-      $(el).append('<div class="row"></div>');
-      var row = $(el).find('.row').last();
-      for(var j = 0; j < 9; j++){
-        row.append('<div class="cell" data-id="' + i + j + '"><div class="cell-inner"></div></div>');
-        var square = this.grid[i][j];
-        var cell = $('div').find('[data-id="' + i + j + '"]');
-        if(square.revealed){
-          cell.addClass('revealed').find('.cell-inner').append(square.curValue);
-        } else{
-          cell.addClass('guessed').prepend('<div class="notes"></div>');
-        }
-      }
-    }
-  };
-
-  // temporary board generator function just to populate with numbers
-  Board.prototype.populate = function(){
-    var value, revealed, pos;
-    var masterBoard = [];
-    for(var i = 0; i < 9; i++){
-      masterBoard.push(_.zip(MASTERROWS[i], SHOWN[i]));
-    }
-
-    for(var i = 0; i < 9; i++){
-      for(var j = 0; j < 9; j++){
-        value = masterBoard[i][j][0];
-        revealed = masterBoard[i][j][1];
-        pos = [i,j]; 
-        if(revealed){
-          this.setCoord([i,j], new Square(value, value, revealed, pos));
-        } else{
-          this.setCoord([i,j], new Square("", value, revealed, pos));
-        }
-      }
-    }
-  };
-
-  Board.prototype.updateCell = function(id, target){
-    if(this.deleteMode){
-      this.setCoord([id[0], id[1]], '');
-      $(target).html("").parent()
-        .removeClass('highlighted')
-        .removeClass('incorrect')
-        .prepend('<div class="notes"></div');
-    } else if(this.selectedNumber){
-      this.setCoord([id[0], id[1]], this.selectedNumber);
-      $(target).html(this.selectedNumber)
-        .parent().addClass('highlighted')
-        .find('.notes').remove();
-    }
-
-    if(this.answerMode){
-      this.flagWrongAnswers();
-    }
-
-    if(this.conflictMode){
-      this.findConflicts();
-    }
-
-    if(this.won()){
-      $('#win').fadeIn();
-    }
-  };
-
-  Board.prototype.highlightCells = function(){
-    $('div.cell.highlighted').removeClass('highlighted');
-    for(var i = 0; i < this.grid.length; i++){
-      for(var j = 0; j < this.grid[i].length; j++){
-        if(this.grid[i][j].curValue === this.selectedNumber){
-          $('#board-container').find('[data-id="' + i + j + '"]').addClass('highlighted');
-        }
-      }
-    }
+    $(target).html(Object.keys(square.notes).join(' '));
   };
 
   Board.prototype.findConflicts = function(){
     $('.conflict').removeClass('conflict');
-    this.findRowConflicts();
-    this.findColumnConflicts();
-    this.findGroupConflicts();
-  };
-
-  Board.prototype.findRowConflicts = function(){
-    var valueMaps = this._mapValues(this.grid);
-    _.each(valueMaps, function(valueMap){
-      this.flagConflicts(valueMap);
-    }, this);
-  };
-
-  Board.prototype.findColumnConflicts = function(){
-    var transposedGrid = _.zip.apply(_, this.grid)
-    var valueMaps = this._mapValues(transposedGrid);
-    _.each(valueMaps, function(valueMap){
-      this.flagConflicts(valueMap);
-    }, this);
-  };
-
-  Board.prototype.findGroupConflicts = function(){
-    var valueMaps = this._mapValues(this.groups());
-    _.each(valueMaps, function(valueMap){
-      this.flagConflicts(valueMap);
-    }, this);
-  };
-
-  Board.prototype._mapValues = function(grid){
-    var valueMap, value;
-    var valueMaps = [];
-    for(var i = 0; i < grid.length; i++){
-      valueMap = {};
-      for(var j = 0; j < grid.length; j++){
-        value = grid[i][j].curValue;
-        if(valueMap[value]){
-          valueMap[value].push(grid[i][j].pos);
-        } else {
-          valueMap[value] = [grid[i][j].pos];
-        }
-      }
-      valueMaps.push(valueMap);
-    }
-    return valueMaps;
+    this._findRowConflicts();
+    this._findColumnConflicts();
+    this._findGroupConflicts();
   };
 
   Board.prototype.flagConflicts = function(valueMap){
@@ -206,6 +79,14 @@ var SHOWN = Sudoku.SHOWN = [
     });
   };
 
+  Board.prototype.full = function() {
+    var flatBoard = _.flatten(this.grid);
+    var allValues = _.map(flatBoard, function(square){
+      return square.curValue;
+    });
+    return allValues.indexOf("") === -1;
+  };
+
   Board.prototype.groups = function(){
     var groups = [];
     for(var i = 0; i <= 6; i += 3){
@@ -221,40 +102,61 @@ var SHOWN = Sudoku.SHOWN = [
     return groups;
   };
 
-  Board.prototype.full = function() {
-    var flatBoard = _.flatten(this.grid);
-    var allValues = _.map(flatBoard, function(square){
-      return square.curValue;
-    });
-    return allValues.indexOf("") === -1;
-  };
-
-  Board.prototype.check = function(array){
-    for(var i = 0; i < array.length; i++){
-      if(_.find(array[i], function(square){
-        return square.curValue !== square.winningValue;
-      })){
-        return false;
+  Board.prototype.highlightCells = function(){
+    $('div.cell.highlighted').removeClass('highlighted');
+    for(var i = 0; i < this.grid.length; i++){
+      for(var j = 0; j < this.grid[i].length; j++){
+        if(this.grid[i][j].curValue === this.selectedNumber){
+          $('#board-container').find('[data-id="' + i + j + '"]').addClass('highlighted');
+        }
       }
-    };
-    return true;
+    }
   };
 
-  Board.prototype.checkRows = function(){
-    return this.check(this.grid);
+  Board.prototype.populate = function(){
+    var value, revealed, pos;
+    var masterBoard = [];
+    for(var i = 0; i < 9; i++){
+      masterBoard.push(_.zip(MASTERROWS[i], SHOWN[i]));
+    }
+
+    for(var i = 0; i < 9; i++){
+      for(var j = 0; j < 9; j++){
+        value = masterBoard[i][j][0];
+        revealed = masterBoard[i][j][1];
+        pos = [i,j]; 
+        if(revealed){
+          this.setCoord([i,j], new Square(value, value, revealed, pos));
+        } else{
+          this.setCoord([i,j], new Square("", value, revealed, pos));
+        }
+      }
+    }
   };
 
-  Board.prototype.checkColumns = function(){
-    var transposedGrid = _.zip.apply(_, this.grid);
-    return this.check(transposedGrid);
+  Board.prototype.render = function(el){
+    for(var i = 0; i < this.grid.length; i++){
+      $(el).append('<div class="row"></div>');
+      var row = $(el).find('.row').last();
+      for(var j = 0; j < 9; j++){
+        row.append('<div class="cell" data-id="' + i + j + '"><div class="cell-inner"></div></div>');
+        var square = this.grid[i][j];
+        var cell = $('div').find('[data-id="' + i + j + '"]');
+        if(square.revealed){
+          cell.addClass('revealed').find('.cell-inner').append(square.curValue);
+        } else{
+          cell.addClass('guessed').prepend('<div class="notes"></div>');
+        }
+      }
+    }
   };
 
-  Board.prototype.checkGroups = function(){
-    return this.check(this.groups());
-  };
-
-  Board.prototype.won = function(){
-    return this.full() && this.checkRows() && this.checkColumns() && this.checkGroups();
+  Board.prototype.setCoord = function(arr, val){
+    if(typeof this.grid[arr[0]][arr[1]] == "object"){
+      this.grid[arr[0]][arr[1]].curValue = val;
+    } else {
+      this.grid[arr[0]][arr[1]] = val;
+    }
   };
 
   Board.prototype.selectButton = function(target){
@@ -264,6 +166,7 @@ var SHOWN = Sudoku.SHOWN = [
     this.selectedNumber = parseInt($(target).val());
     this.highlightCells();
   };
+
 
   Board.prototype.toggleAnswerMode = function(){
     this.answerMode ? this.answerMode = false : this.answerMode = true;
@@ -299,17 +202,107 @@ var SHOWN = Sudoku.SHOWN = [
     }
   };
 
-  Board.prototype.addNote = function(id, target){
-    if(!this.selectedNumber){
-      return;
+  Board.prototype.updateCell = function(id, target){
+    if(this.deleteMode){
+      this.setCoord([id[0], id[1]], '');
+      $(target).html("").parent()
+        .removeClass('highlighted')
+        .removeClass('incorrect')
+        .prepend('<div class="notes"></div');
+    } else if(this.selectedNumber){
+      this.setCoord([id[0], id[1]], this.selectedNumber);
+      $(target).html(this.selectedNumber)
+        .parent().addClass('highlighted')
+        .find('.notes').remove();
     }
-    var square = this.grid[id[0]][id[1]];
-    if(square.notes[this.selectedNumber]){
-      delete square.notes[this.selectedNumber]
-    } else {
-      square.notes[this.selectedNumber] = true;
+
+    if(this.answerMode){
+      this.flagWrongAnswers();
     }
-    $(target).html(Object.keys(square.notes).join(' '));
+
+    if(this.conflictMode){
+      this.findConflicts();
+    }
+
+    if(this.won()){
+      $('#win').fadeIn();
+    }
+  };
+
+  Board.prototype.won = function(){
+    return this.full() && this.checkRows() && this.checkColumns() && this.checkGroups();
+  };
+
+  Board.prototype._check = function(array){
+    for(var i = 0; i < array.length; i++){
+      if(_.find(array[i], function(square){
+        return square.curValue !== square.winningValue;
+      })){
+        return false;
+      }
+    };
+    return true;
+  };
+
+  Board.prototype._checkRows = function(){
+    return this.check(this.grid);
+  };
+
+  Board.prototype._checkColumns = function(){
+    var transposedGrid = _.zip.apply(_, this.grid);
+    return this.check(transposedGrid);
+  };
+
+  Board.prototype._checkGroups = function(){
+    return this.check(this.groups());
+  };
+
+  Board.prototype._findRowConflicts = function(){
+    var valueMaps = this._mapValues(this.grid);
+    _.each(valueMaps, function(valueMap){
+      this.flagConflicts(valueMap);
+    }, this);
+  };
+
+  Board.prototype._findColumnConflicts = function(){
+    var transposedGrid = _.zip.apply(_, this.grid)
+    var valueMaps = this._mapValues(transposedGrid);
+    _.each(valueMaps, function(valueMap){
+      this.flagConflicts(valueMap);
+    }, this);
+  };
+
+  Board.prototype._findGroupConflicts = function(){
+    var valueMaps = this._mapValues(this.groups());
+    _.each(valueMaps, function(valueMap){
+      this.flagConflicts(valueMap);
+    }, this);
+  };
+
+  Board.prototype._generateGrid = function(){
+    var grid = new Array(9);
+    for(var i = 0; i < grid.length; i++){
+      grid[i] = new Array(9);
+    }
+    return grid;
+  };
+
+  Board.prototype._mapValues = function(grid){
+    var valueMap, value;
+    var valueMaps = [];
+    for(var i = 0; i < grid.length; i++){
+      valueMap = {};
+      for(var j = 0; j < grid.length; j++){
+        value = grid[i][j].curValue;
+        if(valueMap[value]){
+          valueMap[value].push(grid[i][j].pos);
+        } else {
+          valueMap[value] = [grid[i][j].pos];
+        }
+      }
+      valueMaps.push(valueMap);
+    }
+    return valueMaps;
   };
 
   var Square = Sudoku.Square = function(curValue, winningValue, revealed, pos){
@@ -355,6 +348,7 @@ $(document).ready(function(){
     $('button.number-button').removeClass('selected');
     $('button#notes-button').removeClass('selected');
     S.noteMode = false;
+    $('.notes').removeClass('forward');
     S.toggleDeleteMode();
   });
 
